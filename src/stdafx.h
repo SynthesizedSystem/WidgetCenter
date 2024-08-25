6b9b8f7d88eaf44fe8009c48ca647da5570e3cd1
Max Ivanov <maks.ivanov.developer@gmail.com>
2024-08-25 19:56:14 +0200

    Add project files.

---
+++ src/stdafx.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <algorithm>
+#include <array>
+#include <chrono>
+#include <cstdint>
+#include <cstdlib>
+#include <fstream>
+#include <functional>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <memory>
+#include <numeric>
+#include <optional>
+#include <set>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+#include <Windows.h>
